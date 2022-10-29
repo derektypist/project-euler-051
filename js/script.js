f509848ec6eb8f5a78102cfbcce50660d39466cb
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-29 10:20:22 +0000

    Added primeDigitReplacements function at script.js

--- js/script.js
+++ js/script.js
@@ -45,3 +45,45 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+/*
+    Function to Return the Solution to the Problem as described in
+    https://projecteuler.net/problem=51
+
+    primeDigitReplacements(6) returns 13
+    primeDigitReplacements(7) returns 56003
+    primeDigitReplacements(8) returns 121313
+*/
+function primeDigitReplacements(n) {
+    // Create new instance of PrimeSieve
+    const primeSieve = new PrimeSieve(n * n * n * 2000);
+
+    function isNFamily(number, n) {
+        const prime = number.toString();
+        const lastDigit = prime[prime.length - 1];
+        return doesReplacingMakeFamily(prime,'0',n) || doesReplacingMakeFamily(prime,'2',n) || (lastDigit !== '1' && doesReplacingMakeFamily(prime,'1',n));
+    }
+
+    function doesReplacingMakeFamily(prime,digitToReplace,family) {
+        let miss = 0;
+        const base = parseInt(prime.split('').map(digit => digit == digitToReplace ? '0' : digit).join(''));
+        const replacements = parseInt(prime.split('').map(digit => digit === digitToReplace ? '1' : '0').join(''));
+        const start = prime[0] === digitToReplace ? 1 : 0;
+        for (let i=start;i<10;i++) {
+            const nxtNumber = base + i*replacements;
+            if (!isPartOfFamily(nxtNumber, prime)) miss++;
+            if (10 - start - miss < family) break;
+        }
+        return 10 - start - miss === family;
+    }
+
+    function isPartOfFamily(number, prime) {
+        return primeSieve.isPrime(number) && number.toString().length === prime.length;
+    }
+
+    for (let number=1;number<125000;number++) {
+        if (primeSieve.isPrime(number) && isNFamily(number, n)) return number;
+    }
+
+    return -1;
+}
