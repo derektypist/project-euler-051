e554c9251eae592e5992ba6ba45f8a14a8176a74
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-29 10:21:15 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -87,3 +87,9 @@ function primeDigitReplacements(n) {
 
     return -1;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
