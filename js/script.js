4d859d52f5253ba49be39c0432073430485e3d5d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-29 09:59:30 +0000

    Added getNumberInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -27,3 +27,21 @@ class PrimeSieve {
         return this._sieve[parseInt((num-3)/2)];
     }
 };
+
+// Function to Get Number Information (including Invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the Input Field
+    let num = document.getElementById("mynumber").value;
+    // Check if the input is valid
+    if (isNaN(num) || num.length==0 || num<2 || num>8 || (num.length>1 && num[0] == "0") || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 2 and 8.  Do not include leading zeros.`;
+    } else {
+        txt += `You have requested ${num} primes. <p>`;
+        txt += `Smallest prime is ${primeDigitReplacements(num)}.`;
+    }
+
+    // Display Information in the Browser
+    document.getElementById("numinfo").innerHTML = txt;
+}
